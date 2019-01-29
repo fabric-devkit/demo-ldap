9dc24ef9da3f2b38363c0d7d40f57c8e5a320bdb
dmcarrington@googlemail.com <dmcarrington@googlemail.com>
2019-01-29 20:53:12 +0000

    fixed addUser, added showUsers

--- addUser.sh
+++ addUser.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 2 ];
 fi
 
 cat >/tmp/newUser.ldif <<EOF
-dn: cn=Dave,dc=example,dc=org
+dn: cn=$1,dc=example,dc=org
 changetype: add
 cn: $1
 sn: $2
@@ -15,4 +15,4 @@ objectClass: person
 objectClass: top
 EOF
 
-docker exec -b my-openldap-container ldapadd -x -w "adminpw" -D "cn=admin,dc=example,dc=org" -f /tmp/newUser.ldif
+docker exec my-openldap-container ldapadd -x -w "adminpw" -D "cn=admin,dc=example,dc=org" -f /tmp/newUser.ldif
