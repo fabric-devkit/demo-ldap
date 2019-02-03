d5616fc7fdfcf490d6bb9f8b2609c6fbc5d9dc9f
dmcarrington@googlemail.com <dmcarrington@googlemail.com>
2019-02-03 17:47:18 +0000

    fixed enrolment. Updated scripts to set passwords for new users and enrol in CA

--- addUser.sh
+++ addUser.sh
@@ -6,9 +6,9 @@ if [ "$#" -ne 2 ];
 fi
 
 cat >/tmp/newUser.ldif <<EOF
-dn: cn=$1,dc=example,dc=org
+dn: cn=$1$2,dc=example,dc=org
 changetype: add
-cn: $1
+cn: $1$2
 sn: $2
 objectClass: organizationalPerson
 objectClass: person
@@ -16,3 +16,4 @@ objectClass: top
 EOF
 
 docker exec my-openldap-container ldapadd -x -w "adminpw" -D "cn=admin,dc=example,dc=org" -f /tmp/newUser.ldif
+docker exec -it my-openldap-container ldappasswd -x -w "adminpw" -D "cn=admin,dc=example,dc=org" -S "cn=$1$2,dc=example,dc=org"
--- showUsers.sh
+++ showUsers.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # List all users in the LDAP server
-docker exec my-openldap-container ldapsearch -x -H ldap://localhost -b dc=example,dc=org -D "cn=admin,dc=example,dc=org" -w adminpw
+docker exec my-openldap-container ldapsearch -x -L -H ldap://localhost -b dc=example,dc=org -D "cn=admin,dc=example,dc=org" -w adminpw
