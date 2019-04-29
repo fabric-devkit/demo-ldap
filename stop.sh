f81c2d2965ba45be58448d32c3d1c00e3adbc24a
David Carrington <dmcarrington@googlemail.com>
2019-04-29 20:58:12 +0100

    Add our new docker containers to start and stop scripts

--- stop.sh
+++ stop.sh
@@ -7,5 +7,13 @@
 # Exit on first error, print all commands.
 set -ev
 
+# Stop any of our SDK or webApp servers
+if docker ps | grep hlf-sdk-server; then
+   docker stop hlf-sdk-server && docker rm -f hlf-sdk-server
+fi
+if docker ps | grep hlf-webapp-server; then
+   docker stop hlf-webapp-server && docker rm -f hlf-webapp-server
+fi
+
 # Shut down the Docker containers that might be currently running.
 docker-compose -f docker-compose.yml down
